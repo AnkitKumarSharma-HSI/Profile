8372e46cc6c6f31b11983f7ef1764488bdc3d587
Ankit-kumar-sharma <ankitkumarsharmacse@gmail.com>
2022-01-06 15:45:57 +0530

    work update

--- JavaScript/myJs.js
+++ JavaScript/myJs.js
@@ -1,19 +1,14 @@
-function mySideBar()
-{
-    let barsClass=document.getElementById('bars');
-    if(barsClass.name=='reorder-three-outline')
-    {
-        barsClass.name='close-outline';
-        let sideBar=document.getElementsByClassName('sideBar')[0];
-        sideBar.style="left:0%;";
-        console.log(sideBar);
-    }
-    else
-    {
-        barsClass.name='reorder-three-outline';
-        let sideBar=document.getElementsByClassName('sideBar')[0];
-        sideBar.style="left:-100%;";
-        
-    }
-    // alert("Hello It's Working");
+function mySideBar() {
+  let barsClass = document.getElementById("bars");
+  if (barsClass.name == "reorder-three-outline") {
+    barsClass.name = "close-outline";
+    let sideBar = document.getElementsByClassName("sideBar")[0];
+    sideBar.style = "left:0%;";
+    console.log(sideBar);
+  } else {
+    barsClass.name = "reorder-three-outline";
+    let sideBar = document.getElementsByClassName("sideBar")[0];
+    sideBar.style = "left:-100%;";
+  }
+  // alert("Hello It's Working");
 }
